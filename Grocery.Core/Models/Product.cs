9c2d98c3a1cac67b1036a8a77721ed5645b8fb0e
Wout van Gurp <wout@brightflow.nl>
2025-09-23 19:21:48 +0200

    zoekbalk gerealiseerd zoek algoritme gerealiseerd zoek algoritme getest refactor code

--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -3,9 +3,12 @@
     public class Product : Model
     {
         public int Stock { get; set; }
+        public string Name { get; set; }
+
         public Product(int id, string name, int stock) : base(id, name)
         {
             Stock = stock;
+            Name = name;
         }
     }
 }
